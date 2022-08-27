c5fb877f2efc1019ef2e8370a82d3c81c2fe9a8d
Grant Jurgensen <grant@jurgensen.dev>
2022-08-27 16:43:05 -0500

    Add latexc function; default to tmux attach

--- .rc.sh
+++ .rc.sh
@@ -7,6 +7,17 @@ viewcsv(){
   cat $1 | sed -e 's/,/ ,/g' | column -t -s, | less -S
 }
 
+latexc(){
+  if [ -z "$(which pdflatex)" ]; then
+    echo "pdflatex not found"
+  elif [ -z "$(which bibtex)" ]; then
+    echo "bibtex not found"
+  else
+    target=${1%.tex}
+    pdflatex $target && bibtex $target && pdflatex $target && pdflatex $target
+  fi
+}
+
 alias emacs='emacs -nw'
 
 export TERM=xterm-256color
@@ -16,5 +27,5 @@ source ~/.rc_local.sh
 # https://unix.stackexchange.com/questions/480451/how-to-start-tmux-upon-running-terminal
 # get into tmux at start
 # if [ -z "$TMUX" ]; then
-#   tmux new-session
+#   tmux attach || tmux new-session
 # fi
