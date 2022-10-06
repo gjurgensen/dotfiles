3c5b4589efaf2d72fbecd5807180886d7d6e4ac8
Grant Jurgensen <grant@jurgensen.dev>
2022-10-06 15:40:57 -0500

    Add zsh install flags

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -4,9 +4,13 @@ sudo apt update
 sudo apt install zsh stow vim neovim tmux emacs git curl -y
 
 # install zsh
+export CHSH=yes
+export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install vundle
 [ -d ~/.vim/bundle/Vundle.vim ] || git clone https://github.com/VundleVim/Vundle.vim.git ~/vim/bundle/Vundle.vim
 
-stow .
+stow --adopt .
+
+exec zsh
