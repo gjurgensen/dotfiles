4c34d8941adc8ee50374424bf17c6211c2232ee9
Grant Jurgensen <grant@jurgensen.dev>
2022-11-10 10:12:53 -0600

    Separate emacs custom

---
+++ .emacs.d/custom.el
@@ -0,0 +1,34 @@
+;; Auto-generated
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(proof-general evil-collection use-package evil cmake-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-variable-tag ((t (:foreground "#83A598" :weight bold))))
+ '(font-lock-builtin-face ((t (:foreground "#D3869B"))))
+ '(font-lock-comment-face ((t (:foreground "#928874" :slant italic))))
+ '(font-lock-constant-face ((t (:foreground "#83A598"))))
+ '(font-lock-function-name-face ((t (:foreground "#8EC07C"))))
+ '(font-lock-keyword-face ((t (:foreground "#FB4934"))))
+ '(font-lock-string-face ((t (:foreground "#B8BB26"))))
+ '(font-lock-variable-name-face ((t (:foreground "#FABD2F"))))
+ '(line-number ((t (:inherit nil :foreground "#928874"))))
+ '(line-number-current-line ((t (:inherit line-number))))
+ '(minibuffer-prompt ((t (:foreground "#B8BB26"))))
+ '(mode-line ((t (:background "#504945" :foreground "#EBDBB2" :box (:line-width -1 :style released-button)))))
+ '(mode-line-inactive ((t (:background "#3c3836" :foreground "#A89984" :box (:line-width -1 :style released-button)))))
+ '(proof-locked-face ((t (:extend t :background "color-237"))))
+ '(vertical-border ((t (:foreground "#928874"))))
+ '(whitespace-indentation ((t (:inherit whitespace-space))))
+ '(whitespace-newline ((t (:foreground "#928874" :weight normal))))
+ '(whitespace-space ((t (:foreground "#928874"))))
+ '(whitespace-tab ((t (:foreground "#928874"))))
+ '(window-divider ((t (:foreground "#928874")))))
