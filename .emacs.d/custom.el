d29bdf7f44d40e0488978507e016e0017af9ac3d
Grant Jurgensen <grant@jurgensen.dev>
2024-08-07 10:31:01 -0500

    [emacs] Color scheme

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,10 +5,27 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(Man-notify-method 'pushy))
+ '(Man-notify-method 'pushy)
+ '(package-selected-packages
+   '(rust-mode copilot quelpa json-rpc ## editorconfig xclip use-package treemacs-evil smartparens rainbow-delimiters proof-general pkg-info markdown-mode magit haskell-mode flycheck eyebrowse exec-path-from-shell evil-org evil-collection doom-modeline company-coq cmake-mode autothemer auto-package-update all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ansi-color-black ((t (:background "#282828" :foreground "#282828"))))
+ '(ansi-color-blue ((t (:background "#458588" :foreground "#458588"))))
+ '(ansi-color-bright-black ((t (:background "#665c54" :foreground "#665c54"))))
+ '(ansi-color-bright-blue ((t (:background "#83a598" :foreground "#83a598"))))
+ '(ansi-color-bright-cyan ((t (:background "#8ec07c" :foreground "#8ec07c"))))
+ '(ansi-color-bright-green ((t (:background "#b8bb26" :foreground "#b8bb26"))))
+ '(ansi-color-bright-magenta ((t (:background "#d3869b" :foreground "#d3869b"))))
+ '(ansi-color-bright-red ((t (:background "#fb4934" :foreground "#fb4934"))))
+ '(ansi-color-bright-white ((t (:background "#fbf1c7" :foreground "#fbf1c7"))))
+ '(ansi-color-bright-yellow ((t (:background "#fabd2f" :foreground "#fabd2f"))))
+ '(ansi-color-cyan ((t (:background "#689d6a" :foreground "#689d6a"))))
+ '(ansi-color-green ((t (:background "#98971A" :foreground "#98971A"))))
+ '(ansi-color-magenta ((t (:background "#b16286" :foreground "#b16286"))))
+ '(ansi-color-red ((t (:background "#CC241D" :foreground "#CC241D"))))
+ '(ansi-color-white ((t (:background "#3c3836" :foreground "#ebdbb2"))))
+ '(ansi-color-yellow ((t (:background "#d79921" :foreground "#d79921")))))
